092cb1254642ff72adbf331ddd0787b1b07bb48b
Joanne <jeffiefeifei@gmail.com>
2017-02-02 23:15:22 -0800

    [add] prototype and dunder-proto

--- constructor_function.js
+++ constructor_function.js
@@ -10,3 +10,11 @@ function User(firstName, lastName, age, gender) {
 // user1 and user200 are an objects of the class User.
 var user1 = new User('John', 'Smith', 26, 'male');
 var user200 = new User('Jill', 'Robinson', 25, 'female');
+
+
+// add emailDomain property on User prototype
+// if we check user1 or user200, we can access emailDomain property, and getEmailAddress function under dunder-proto object
+User.prototype.emailDomain = '@facebook.com';
+User.prototype.getEmailAddress = function() {
+  return this.firstName + this.lastName + this.emailDomain;
+}
